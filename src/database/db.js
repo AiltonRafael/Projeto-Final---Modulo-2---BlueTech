d7055ccf829ca3ba60ac3469fe478bd3242eadbd
Ailton Rafael <ailton.junior@al.infnet.edu.br>
2022-05-02 12:16:26 -0300

    updated

--- src/database/db.js
+++ src/database/db.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const connection = new Sequelize(
-    // process.env.DATABASE_URL,
     "postgres://pokemon_82jv_user:DEZQDhcVKeM4mCf6WDhAQGlES1HufziQ@dpg-c9nu0ec41ls5dvtp19b0-a.oregon-postgres.render.com/pokemon_82jv",
     {
     dialect: 'postgres',
